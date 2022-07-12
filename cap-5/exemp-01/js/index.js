ff9ece415a12550bacb74c8e90880f62660e3ca1
Henrique Rodrigues Santana <henrique.rocdrigue@gmail.com>
2022-07-12 17:33:04 -0400

    exemp-01-cap-5

---
+++ cap-5/exemp-01/js/index.js
@@ -0,0 +1,69 @@
+
+var clientes = []
+
+
+
+function adicionar(){
+
+    var inppaciente = document.getElementById('inpPaciente').value
+   
+    if(inppaciente === ' '){
+        alert('Campo vazio, favor adicionar umcliente')
+        return
+    }
+
+    clientes.push(inppaciente)
+
+    outAtender.textContent =`${clientes[0]}`
+    outPaciente.textContent=`${clientes.join('\n')}`
+    console.log(clientes.toString())
+
+
+}
+
+function urgencia(){
+
+    var inppaciente = document.getElementById('inpPaciente').value
+   
+    if(inppaciente === ' '){
+        alert('Campo vazio, favor adicionar umcliente')
+        return
+    }
+
+    clientes.unshift(inppaciente)
+
+    outAtender.textContent =`${clientes[0]}`
+    outPaciente.textContent=`${clientes.join('\n')}`
+    console.log(clientes.toString())
+
+
+}
+
+function atender(){
+
+    var inppaciente = document.getElementById('inpPaciente').value
+   
+    if(inppaciente === ' '){
+        alert('Campo vazio, favor adicionar umcliente')
+        return
+    }
+    
+    
+
+    outAtender.textContent =`${clientes[0]}`
+
+    clientes.shift(clientes[0])
+    outPaciente.textContent=`${clientes.join('\n')}`
+    console.log(clientes.toString())
+
+
+}
+
+var btAdicionar = document.getElementById('btAdicionar')
+var bturgencia = document.getElementById('bturgencia')
+var btAtender = document.getElementById('btAtender')
+
+
+btAdicionar.addEventListener('click', adicionar )
+bturgencia.addEventListener('click', urgencia)
+btAtender.addEventListener('click', atender)
